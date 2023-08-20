9fc10437912aa7e741f111f1ba768508db9269ac
Maya <maya88en@gmail.com>
2023-08-20 11:55:42 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
 /**
  * _isdigit - function that checks for 0 through 9
  * @c: parameter to be checked
- * Return 1 on success 0 on failure
+ * Return: 1 on success and 0 on failure
  */
 
 int _isdigit(int c)
